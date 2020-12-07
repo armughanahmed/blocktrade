aafcdd3d4436bcd67014eee8f2dde8cb4a598e62
Muhammad Farhan <farhan.fahim010@gmail.com>
2020-12-07 09:46:43 +0500

    routes added in app.js

--- Frontend/src/App.js
+++ Frontend/src/App.js
@@ -10,8 +10,9 @@ import {
 } from "react-router-dom";
 import Register from './layouts/Register';
 import Login from './layouts/Login';
-import DashboardCO from './layouts/DashboardCO';
-
+import DashboardCO from './layouts/cargoOwner/DashboardCO';
+import StartConsignmentCO from './layouts/cargoOwner/StartConsignmentCO';
+import ViewConsignmentsCO from './layouts/cargoOwner/ViewConsignmentsCO';
 
 function App() {
   return (  
@@ -20,6 +21,8 @@ function App() {
           <Route path="/login" component={Login}/>
           <Route path="/register" component={Register}/>
           <Route path="/dashboard" component={DashboardCO}/>
+          <Route path="/startConsignment" component={StartConsignmentCO}/>
+          <Route path="/viewConsignments" component={ViewConsignmentsCO}/>
       </Switch>
     </Router>
   );
